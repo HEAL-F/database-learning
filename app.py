066ece984d28405308e267102f0384f19026feff
HEAL-F <andrijperig81@gmail.com>
2025-11-07 23:38:30 +0200

    Base

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('hello world')
+import tkinter as tk
+from tkinter import messagebox
+
+def on_button_click():
+    messagebox.showinfo("Information", "Button was clicked!")
+root = tk.Tk()
+root.title("Simple Tkinter App")
+root.geometry("300x200")
+button = tk.Button(root, text="Click Me", command=on_button_click)
+button.pack(pady=50, padx=100, fill='x', expand=True)
+root.mainloop()
